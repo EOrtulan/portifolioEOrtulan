7f07123b60289ba4f0063ea74c7777f56430523d
EOrtulan <eduortulan@gmail.com>
2025-05-16 23:40:29 -0300

    adicionando resumo sobre mim

--- src/Pages/sobre.jsx
+++ src/Pages/sobre.jsx
@@ -5,7 +5,7 @@ function Sobre() {
   return (
     <div className="pt-16">
       <BarraNavega />
-      <div className="bg-gray-950 rounded-xl shadow-lg p-6 md:p-10 max-w-5xl mx-auto mt-10 ">
+      <div className="bg-gray-900 rounded-xl shadow-lg p-6 md:p-10 max-w-5xl mx-auto mt-10 ">
         <h2 className="text-2xl font-semibold mb-6 border-b border-emerald-800 inline-block">
           Sobre mim
         </h2>
@@ -13,14 +13,18 @@ function Sobre() {
           <img
             src="/Public/minhaFoto.jpg"
             alt="Avatar"
-            className="w-40 h-40 rounded-full object-cover shadow-md"
+            className="w-40 aspect-square rounded-full object-cover shadow-md"
           />
           <div className="text-sm md:text-base space-y-4">
             <p>
-              oi
+            Sou formado em Desenvolvimento Web pela Trybe, uma escola de tecnologia que oferece formação intensiva com foco
+             em habilidades técnicas e comportamentais.
+             Durante minha trajetória, desenvolvi projetos práticos nas áreas de Front-End e Back-End, utilizando tecnologias
+              como HTML, CSS, JavaScript, React, Node.js e MySQL.
             </p>
             <p>
-         oi
+            Embora ainda não tenha atuado profissionalmente na área, 
+            estou em constante aprendizado e aprimoramento, buscando minha primeira oportunidade como desenvolvedor.
             </p>
           </div>
         </div>
